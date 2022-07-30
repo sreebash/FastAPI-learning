81d970bfe2044db05afe1d7ae910a59fa0479de0
sreebash <sreebashdas.1@gmail.com>
2022-07-30 13:33:42 +0600

    add sqlalchemy database connection and declared Blog models

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from blog import models
+from blog.database import engine, SessionLocal
+from blog.schemas import Blog
 
 app = FastAPI()
-
 
 # @app.get("/")
 # async def root():
@@ -15,36 +19,52 @@ app = FastAPI()
 # async def say_hello(name: str):
 #     return {"message": f"Hello {name}"}
 
+#
+# @app.get('/blog')
+# def index(limit=10, published: bool = True, sort: Optional[str] = None):
+#     # return published
+#     if published:
+#         return {'data': f'{limit} published blogs from the database'}
+#     return {'data': f'{limit} blogs from the database'}
+#
+#
+# @app.get('/blog/{unpublished}')
+# def unpublished(unpublished: str):
+#     return {'data': unpublished}
+#
+#
+# @app.get('/blog/{blog_id}')
+# def show_blog(blog_id: int):
+#     return {'data': blog_id}
+#
+#
+# @app.get('/blog/{comments_id}/comments')
+# def comments(comments_id, limit: int = 10):
+#     return limit
+#
+#
+# class Blog(BaseModel):
+#     title: str
+#     body: str
+#     published_at: Optional[bool]
+#
 
-@app.get('/blog')
-def index(limit=10, published: bool = True, sort: Optional[str] = None):
-    # return published
-    if published:
-        return {'data': f'{limit} published blogs from the database'}
-    return {'data': f'{limit} blogs from the database'}
+# @app.post('/blog')
+# def create_blog(blog: Blog):
+#     return {'data': f'Blog is created with {blog.title}'}
 
 
-@app.get('/blog/{unpublished}')
-def unpublished(unpublished: str):
-    return {'data': unpublished}
+models.Base.metadata.create_all(engine)
 
 
-@app.get('/blog/{blog_id}')
-def show_blog(blog_id: int):
-    return {'data': blog_id}
-
-
-@app.get('/blog/{comments_id}/comments')
-def comments(comments_id, limit: int = 10):
-    return limit
-
-
-class Blog(BaseModel):
-    title: str
-    body: str
-    published_at: Optional[bool]
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.post('/blog')
-def create_blog(request: Blog):
-    return {'data': 'Blog is created'}
+def create(request: Blog, db: Session = Depends(get_db)):
+    return db
