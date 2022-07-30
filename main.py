67b2c5256b1e24b07cd49c41508b6b63b88fbffa
sreebash <sreebashdas.1@gmail.com>
2022-07-30 06:06:22 +0600

    feat: add .gitignore file and proceed to learn FastAPI

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
+
+
+@app.get('/')
+def index():
+    return {'data': {'name': 'Hello FastAPI'}}
+
+
+@app.get('/about')
+def about():
+    return {'data': 'about page'}
