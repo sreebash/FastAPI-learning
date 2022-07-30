666e057477f2a59a91884bc6a201f8f1acd3883f
sreebash <sreebashdas.1@gmail.com>
2022-07-30 09:21:26 +0600

    add multiple  queryparams accepted at index route

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,11 +15,24 @@ app = FastAPI()
 #     return {"message": f"Hello {name}"}
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Hello FastAPI'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # return published
+    if published:
+        return {'data': f'{limit} published blogs from the database'}
+    return {'data': f'{limit} blogs from the database'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/{unpublished}')
+def unpublished(unpublished: str):
+    return {'data': unpublished}
+
+
+@app.get('/blog/{blog_id}')
+def show_blog(blog_id: int):
+    return {'data': blog_id}
+
+
+@app.get('/blog/{comments_id}/comments')
+def comments(comments_id):
+    return {'data': {'1', '2'}}
