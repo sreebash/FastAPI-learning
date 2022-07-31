a310b0dfc09904a1122c70c7830085327f0937e4
sreebash <sreebashdas.1@gmail.com>
2022-07-31 09:39:58 +0600

    retrieve single blog post and add cusome status code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from starlette import status
 
 from blog import models
 from blog.database import engine, SessionLocal
@@ -65,11 +66,25 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
-    print(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def get_blog_list(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_single_blog(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'detail': f'Blog with the id {id} is not available'}
+    return blog
